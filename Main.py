e3f3e01a014e6aa7c1bffb76158d52c8d63e2e34
CWestray <westraycolter@gmail.com>
2026-02-07 12:43:04 -0500

    Cleanup Main.py and make use of new Window class

--- Main.py
+++ Main.py
@@ -1,26 +1,8 @@
-from tkinter import *
+from AudioController import AudioController
+from Window import Window
 
-def donothing():
-    i = 0
+# Persistent/Singleton AudioController
+audioController = AudioController()
 
-# Setup application window
-mainWindow = Tk()
-mainWindow.title('SoundSight')
-
-mainWindow.geometry("1280x720")
-mainWindow.config(background = "black")
-
-# Create menu bar
-menubar = Menu(mainWindow)
-filemenu = Menu(menubar, tearoff=0)
-menubar.add_cascade(label="File", menu = filemenu)
-
-filemenu.add_command(label="Open File", command=donothing)
-filemenu.add_command(label="Open Directory", command=donothing)
-filemenu.add_command(label="Close File", command=donothing)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=mainWindow.quit)
-
-mainWindow.config(menu = menubar)
-
-mainWindow.mainloop()
+window = Window(audioController)
+window.MainWindow.mainloop()
