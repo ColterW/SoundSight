e2034be53be1b560f45e49c217a959887dfb09e4
CWestray <westraycolter@gmail.com>
2026-02-07 13:32:25 -0500

    Add docstrings for AudioController

--- AudioController.py
+++ AudioController.py
@@ -8,7 +8,13 @@ class AudioController:
         self.SampleRate = None
         self.Volume = 0.1 # 10% volume by default; trust me, it's necessary
 
-    def LoadFile(self, path):
+    def LoadFile(self, path: str) -> None:
+        """
+        Load an audio file from disk.
+        
+        Args:
+            path (str): Path to the audio file.
+        """
         audio = AudioSegment.from_file(path)
         self.SampleRate = audio.frame_rate
 
@@ -17,7 +23,10 @@ class AudioController:
             samples = samples.reshape((-1, 2))
         self.Samples = samples.astype(np.float32) / (2 ** 15)
 
-    def Play(self):
+    def Play(self) -> None:
+        """
+        Play currently loaded audio file.
+        """
         if self.Samples is None:
             return
 
@@ -25,7 +34,18 @@ class AudioController:
         sd.play(scaledSamples, self.SampleRate)
 
     def Stop(self):
+        """
+        Stop playback of currently loaded audio file.
+        """
         sd.stop()
 
-    def SetVolume(self, value):
+    def SetVolume(self, value: float) -> None:
+        """
+        Sets playback volume of currently loaded audio file.
+        
+        Caps minimum volume to 0.0 and maximum volume to 1.0.
+        
+        Args:
+            value (float): New value for audio volume.
+        """
         self.Volume = max(0.0, min(1.0, value))
