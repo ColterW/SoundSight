4b6b2a8763b62ec8c0dc908dec834f29952d5228
CWestray <westraycolter@gmail.com>
2026-02-07 10:39:05 -0500

    Initial commit; basic window display and .gitignore

---
+++ Main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# Setup application window
+mainWindow = Tk()
+mainWindow.title('SoundSight')
+
+mainWindow.geometry("1280x720")
+mainWindow.config(background = "black")
+
+mainWindow.mainloop()
