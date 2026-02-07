c19d75ca0718df97ba129a8bb34d5d431908ac33
CWestray <westraycolter@gmail.com>
2026-02-07 11:39:20 -0500

    Add menu bar and base commands

--- Main.py
+++ Main.py
@@ -1,10 +1,26 @@
 from tkinter import *
 
+def donothing():
+    i = 0
+
 # Setup application window
 mainWindow = Tk()
 mainWindow.title('SoundSight')
 
 mainWindow.geometry("1280x720")
 mainWindow.config(background = "black")
+
+# Create menu bar
+menubar = Menu(mainWindow)
+filemenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="File", menu = filemenu)
+
+filemenu.add_command(label="Open File", command=donothing)
+filemenu.add_command(label="Open Directory", command=donothing)
+filemenu.add_command(label="Close File", command=donothing)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=mainWindow.quit)
+
+mainWindow.config(menu = menubar)
 
 mainWindow.mainloop()
